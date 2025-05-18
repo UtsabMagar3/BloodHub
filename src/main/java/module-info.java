b24a7a015ff7a70c9eafa04f2b3bf90bbccd1a35
UtsabMagar3 <utsabthamimagar@gmail.com>
2025-05-19 00:57:53 +0545

    some changes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module bloodhub {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires mysql.connector.j;
 
     opens bloodhub to javafx.fxml;
     opens controller to javafx.fxml;
